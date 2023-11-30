4f157577ce63e8b6ceaf825818d3b35b5897f4a8
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2023-12-01 05:33:55 +0900

    Title: 전화번호 바꾸기, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 231201/전화번호 바꾸기/change-phone-number.java
+++ 231201/전화번호 바꾸기/change-phone-number.java
@@ -5,6 +5,7 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter("-");
+        int a = sc.nextInt();
         int x = sc.nextInt();
         int y = sc.nextInt();
         System.out.println("010-" + y + "-" + x);
