46ad8e15ebd8c8d37a0d65faad50c97b9078d4df
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-02 08:15:51 +0900

    Title: 남녀노소 구분짓기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240102/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int gen, age;
+        gen = sc.nextInt();
+        age = sc.nextInt();
+
+        if ( gen == 0) {
+            if (age >= 19) {
+                System.out.println("MAN");
+            } else {
+                System.out.println("BOY");
+            }
+        } else {
+            if (age >= 19) {
+                System.out.println("WOMAN");
+            } else {
+                System.out.println("GIRL");
+            }
+        }
+    }
+}
