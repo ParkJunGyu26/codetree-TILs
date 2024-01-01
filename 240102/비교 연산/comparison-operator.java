c3f4486d9dacdb9666730a9b991941900ec1338d
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-02 06:57:24 +0900

    Title: 비교 연산, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

--- 240102/비교 연산/comparison-operator.java
+++ 240102/비교 연산/comparison-operator.java
@@ -1,48 +1,21 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
 
+        // 변수 선언
         int a, b;
+        
+        // 입력
         a = sc.nextInt();
         b = sc.nextInt();
 
-        if(a>=b) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-
-        if(a>b) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-
-        if(b>=a) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-
-        if(b>a) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-
-        if(a==b) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
-
-        if(a!=b) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+        System.out.println(a >= b ? 1 : 0);
+        System.out.println(a > b ? 1 : 0);
+        System.out.println(a <= b ? 1 : 0);
+        System.out.println(a < b ? 1 : 0);
+        System.out.println(a == b ? 1 : 0);
+        System.out.println(a != b ? 1 : 0);
     }
 }
