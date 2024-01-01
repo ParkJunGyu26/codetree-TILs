d636325ab0e367849aa09737afdb4ade6a4ba177
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-02 07:56:10 +0900

    Title: 좀 더 어려운 수학 점수, Time: 233ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240102/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int A_math, A_eng, B_math, B_eng;
+        A_math = sc.nextInt();
+        A_eng = sc.nextInt();
+        B_math = sc.nextInt();
+        B_eng = sc.nextInt();
+
+        if(A_math > B_math || A_math == B_math && A_eng > B_eng) {
+            System.out.println("A");
+        } else {
+            System.out.println("B");
+        }
+    }
+}
