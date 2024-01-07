0c4c4ce1c19e0b8038baf9e60fc0f6a78b676e2c
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-07 17:35:43 +0900

    Title: 건너뛰고 별표 출력하기, Time: 144ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240107/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 1;
+
+        for(int i = 1; i <= (n*2)+1; i++) {
+            for(int j = 1; j <= cnt; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+            if (i < n) cnt++;
+            else cnt--;
+        }
+    }
+}
