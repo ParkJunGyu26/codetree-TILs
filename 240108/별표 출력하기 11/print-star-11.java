b1f89008d7ad28d52966cd8a1c506757cb17c41b
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-08 11:21:00 +0900

    Title: 별표 출력하기 11, Time: 175ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/별표 출력하기 11/print-star-11.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for (int i = 1; i <= (n*2)+1; i++) {
+            for (int j = 1; j <= (n*2)+1; j++) {
+                if (i % 2 == 0 && j % 2 == 0) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
