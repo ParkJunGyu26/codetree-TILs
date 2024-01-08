139b3f59d527f7cb0443fb4709f30d6c6f243aa8
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-08 12:09:54 +0900

    Title: 규칙을 찾아 출력하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240108/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 240108/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -1,27 +1,18 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int star = 1;
 
-        for (int i = 1; i <= n; i++) {
-            if (i == 1 || i == n) {
-                for (int j = 1; j <= n; j++) {
-                    System.out.print("* ");
-                }
-            } else {
-                for (int j = 1; j < star; j++) {
-                    System.out.print("* ");
-                }
-                for (int j = 1; j <= n-star; j++) {
-                    System.out.print("  ");
-                }
-                System.out.print("*");
+        // 변수 선언 및 입력
+        int n = sc.nextInt();
+
+        // n칸의 정사각형에서 i, j가 조건에 맞으면 *을 출력합니다.
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i > j || i == 0 || j == n - 1) System.out.print("* ");
+                else System.out.print("  ");
             }
-            star++;
             System.out.println();
         }
     }
