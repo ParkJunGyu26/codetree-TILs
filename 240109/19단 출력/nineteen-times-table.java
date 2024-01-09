9121300034aecf88225af206591490648f2efedb
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-09 15:50:00 +0900

    Title: 19단 출력, Time: 118ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240109/19단 출력/nineteen-times-table.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for (int i = 1; i <= 19; i++) {
+            for (int j = 1; j <= 19; j++) {
+                if (j == 19) System.out.println(i + " * " + j + " = " + i*j);
+                else if (j % 2 == 1) System.out.print(i + " * " + j + " = " + i*j + " / ");
+                else System.out.println(i + " * " + j + " = " + i*j);
+            }
+        }
+    }
+}
