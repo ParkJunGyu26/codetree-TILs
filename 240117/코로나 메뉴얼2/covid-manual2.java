75678cc4904b8ef43adfb501f744e5b4145296d2
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-17 07:08:39 +0900

    Title: 코로나 메뉴얼2, Time: 122ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240117/코로나 메뉴얼2/covid-manual2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[4];
+        int cnt = 0;
+
+        for (int i = 0; i < 3; i++) {
+            char check = sc.next().charAt(0);
+            int temp = sc.nextInt();
+
+            if (check == 'N') {
+                if (temp >= 37) arr[1]++;
+                else arr[3]++; 
+            } else {
+                if (temp > 37) {
+                    arr[0]++;
+                    cnt++;
+                }
+                else arr[2]++;
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        
+        System.out.print(cnt >= 2 ? 'E' : ' ');
+    }
+}
