f54e8937c7e6fc5632394929a5a2b4942074676f
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-01-17 00:40:43 +0900

    Title: 원소 10개의 합, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240117/원소 10개의 합/sum-of-10-elements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int [] arr = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        int sum = 0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = sc.nextInt();
+            sum += arr[i];
+        }
+
+        System.out.print(sum);
+    }
+}
