0592206d43ea2c73ec7135a95d6a9bdb52be4056
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-02-28 15:31:12 +0900

    Title: n개의 숫자 중 최대 2개, Time: 127ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240228/n개의 숫자 중 최대 2개/two-max-of-n-num.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int firstMax = nums[0];
+        int check = 0;
+
+        for(int i = 0; i < n; i++) {
+            if (firstMax < nums[i]) {
+                firstMax = nums[i];
+                check = i;
+            }
+        }
+
+        int secondMax = nums[0];
+
+        for (int i = 0; i < n; i++) {
+            if (check == i) continue;
+            if (secondMax < nums[i]) secondMax = nums[i];
+        }
+
+        System.out.print(firstMax + " " + secondMax);
+    }
+}
