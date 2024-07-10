23f57db85c7a06f2bb2e7f82fa66be6dc2f134d3
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-10 23:38:44 +0900

    Title: 배열 채우기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240710/배열 채우기/filling-array.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, cnt, num[10];
+    for (int i = 0; i < 10; i++) {
+        cin >> n;
+        if (n == 0) {
+            cnt = i;
+            break;
+        }
+        num[i] = n;
+    }
+
+    for (int i = cnt-1; i >= 0; i--) cout << num[i] << " ";
+
+    return 0;
+}
