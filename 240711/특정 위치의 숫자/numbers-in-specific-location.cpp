c549ba5a4ebac7ab6c1d98e148f27d9018b824a0
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-11 00:29:39 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240711/특정 위치의 숫자/numbers-in-specific-location.cpp
+++ 240711/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -5,12 +5,9 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int num[10];
 
-    int ans = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
         cin >> num[i];
-        ans += num[i];
-    }
 
-    cout << ans;
+    cout << num[2] + num[4] + num[9];
     return 0;
 }
