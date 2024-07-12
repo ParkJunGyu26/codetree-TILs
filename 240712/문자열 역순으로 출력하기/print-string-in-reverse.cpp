dbd2690d0df168c049f2a4278b18f428e2d134d2
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-12 23:44:59 +0900

    Title: 문자열 역순으로 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240712/문자열 역순으로 출력하기/print-string-in-reverse.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string A[4];
+    for (int i = 0; i < 4; i++)
+        cin >> A;
+    
+    for (int i = 3; i >= 0; i--)
+        cout << A[i] << endl;
+
+    return 0;
+}
