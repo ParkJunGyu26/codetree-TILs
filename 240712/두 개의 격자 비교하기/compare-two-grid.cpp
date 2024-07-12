5bae61e852cf9b8fe63a6775f08f1f3265b53a51
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-12 12:47:26 +0900

    Title: 두 개의 격자 비교하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240712/두 개의 격자 비교하기/compare-two-grid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m;
+    cin >> n >> m;
+    
+    int a[10][10], b[10][10], ans[10][10];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> b[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == b[i][j]) ans[i][j] = 0;
+            else ans[i][j] = 1;
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
