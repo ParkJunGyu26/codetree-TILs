c24408804fe010cc2b56143a9961f886f3ce8cf6
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-12 12:01:04 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240712/배열의 합/sum-of-array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num[4][4];
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++)
+            cin >> num[i][j];
+    }
+
+    for (int i = 0; i < 4; i++) {
+        int sum = 0;
+        for (int j = 0; j < 4; j++)
+            sum += num[i][j];
+        cout << sum << endl;
+    }
+
+    return 0;
+}
