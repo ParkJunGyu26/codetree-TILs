2c82dcb87816fc94eef12141898dcfbe1d724f31
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-17 10:19:51 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240717/아스키코드표 맞추기/chart-of-ASCII.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    for (int i = 0; i < 5; i++) {
+        int num;
+        cin >> num;
+        cout << (char) num << " ";
+    }
+
+    return 0;
+}
