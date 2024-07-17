de831ad83791276cf70c365f639c33bd32c0c423
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-18 05:09:48 +0900

    Title: 알파벳 지우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240718/알파벳 지우기/remove-alphabet.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string A, B;
+
+    cin >> A >> B;
+    string ans1 = "";
+    string ans2 = "";
+    for (int i = 0; i < A.size(); i++) {
+        if ('0' <= A[i] && A[i] <= '9') ans1 += A[i];
+        if ('0' <= B[i] && B[i] <= '9') ans2 += B[i];
+    }
+
+    cout << stoi(ans1) + stoi(ans2);
+
+    return 0;
+}
