f1e8195cce5623174bf6ceea345e58992473c4fa
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-20 00:49:23 +0900

    Title: 2021년 날짜의 유무, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240720/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+bool check(int M, int D) {
+    switch (M)
+    {
+        case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+            if (1 <= D && D <= 31) return true;
+        case 4: case 6: case 9: case 11:
+            if (1 <= D && D <= 30) return true;
+        case 2:
+            if (1 <= D && D <= 28) return true;
+        return false;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int M, D;
+    cin >> M >> D;
+    if (check(M, D)) cout << "Yes";
+    else cout << "No";
+    return 0;
+}
