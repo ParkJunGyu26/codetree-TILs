6c848752ac818cc8acd7c9ea0dbccdc864dd8a4a
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-07-30 10:55:22 +0900

    Title: Top K 숫자 구하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240730/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX_SIZE 1001
+
+int k, N;
+int A[MAX_SIZE];
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> N >> k;
+    for (int i = 0; i < N; i++) cin >> A[i];
+    sort(A, A+N);
+    cout << A[k-1];
+    return 0;
+}
