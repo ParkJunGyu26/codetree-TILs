21153951d5555ca2b744edcc7ed7ea1cd37fbbe7
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-08-15 20:24:35 +0900

    Title: 코드네임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240815/코드네임/code-name.cpp
+++ 240815/코드네임/code-name.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    tuple<char, int> people[5];
+    tuple<int, char> people[5];
 
     for (int i = 0; i < 5; i++) {
         char name;
         int score;
         cin >> name >> score;
-        people[i] = make_tuple(name, score);
+        people[i] = make_tuple(score, name);
     }
 
     tuple<char, int> answer = *min_element(people, people+5);
     char ansName;
     int ansScore;
-    tie(ansName, ansScore) = answer;
+    tie(ansScore, ansName) = answer;
 
     cout << ansName << " " << ansScore;
 
