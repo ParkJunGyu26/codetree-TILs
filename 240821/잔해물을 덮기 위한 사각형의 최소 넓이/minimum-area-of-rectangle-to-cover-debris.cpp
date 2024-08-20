b76a54b7f13cf08310105ad5b3bae467d5087faa
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-08-21 04:08:43 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 22ms, Memory: 30MB, Status: Wrong Answer - CodeTree

--- 240821/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
+++ 240821/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.cpp
@@ -6,6 +6,11 @@ int max(int a, int b) {
     return b;
 }
 
+int min(int a, int b) {
+    if (a >= b) return b;
+    return a;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     int rec1[2001][2001] = {0}; // 1이면 첫 번째, -1이면 두 번째 사각형
@@ -14,37 +19,41 @@ int main() {
 
     // 첫 번째 사각형
     cin >> x1 >> y1 >> x2 >> y2;
-    for (int y = y1; y < y2; y++) {
-        for (int x = x1; x < x2; x++) {
+    for (int y = y1; y <= y2; y++) {
+        for (int x = x1; x <= x2; x++) {
             rec1[y+1000][x+1000] = 1;
         }
     }
 
     // 두 번째 사각형
     cin >> x1 >> y1 >> x2 >> y2;
-    for (int y = y1; y < y2; y++) {
-        for (int x = x1; x < x2; x++) {
+    for (int y = y1; y <= y2; y++) {
+        for (int x = x1; x <= x2; x++) {
             rec2[y+1000][x+1000] = -1;
         }
     }
 
-    int answer_x, answer_y;
-    answer_x = answer_y = 0;
+    int min_x, max_x, min_y, max_y;
+    max_x = max_y = 0;
+    min_x = min_y = 2000;
 
     for (int y = 0; y < 2001; y++) {
-        bool target_y = false;
-        int target_x = 0;
         for (int x = 0; x < 2001; x++) {
             if (rec1[y][x] == 1 && rec2[y][x] != -1) {
-                target_y = true;
-                target_x++;
+                max_x = max(x, max_x);
+                max_y = max(y, max_y);
+                min_x = min(x, min_x);
+                min_y = min(y, min_y);
             }
         }
-        if (target_y) answer_y++;
-        answer_x = max(answer_x, target_x);
     }
 
-    cout << answer_x * answer_y;
+    // cout << "max x : " << max_x << endl;
+    // cout << "min x : " << min_x << endl;
+    // cout << "max y : " << max_y << endl;
+    // cout << "min y : " << min_y << endl;
+
+    cout << (max_x - min_x) * (max_y - min_y);
 
     return 0;
 }
