1739116eca8acbe701ab0002971b5eab4f6b4e80
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-08-23 10:21:40 +0900

    Title: 만나는 그 순간, Time: 13ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240823/만나는 그 순간/the-moment-we-meet.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <tuple>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m, now, time;
+    cin >> n >> m;
+
+    int A[2000001], B[2000001];
+    for (int i = 0; i < 2000001; i++) {
+        A[i] = -1;
+        B[i] = -1;
+    }
+
+    time = 0;
+    now = 1000000;
+    for (int i = 0; i < n; i++) {
+        char d;
+        int c;
+
+        cin >> d >> c;
+        
+        if (d == 'R') {
+            for (int t = time; t < time+c; t++)
+                A[t] = now++;
+        } else {
+            for (int t = time; t < time+c; t++)
+                A[t] = now--;
+        }
+        time += c;
+    }
+
+    time = 0;
+    now = 1000000;
+    for (int i = 0; i < m; i++) {
+        char d;
+        int c;
+
+        cin >> d >> c;
+        
+        if (d == 'R') {
+            for (int t = time; t < time+c; t++)
+                B[t] = now++;
+        } else {
+            for (int t = time; t < time+c; t++)
+                B[t] = now--;
+        }
+        time += c;
+    }
+
+    for (int i = 1; i < time; i++) {
+        if (A[i] == B[i]) {
+            cout << i;
+            break;
+        }
+    }
+
+    return 0;
+}
