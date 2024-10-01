b8b0c1350a57a4dcae571594da336e09eb7f2f7d
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-10-01 18:16:47 +0900

    Title: 좌우로 움직이는 로봇, Time: 22ms, Memory: 7MB, Status: Passed - CodeTree

--- 241001/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
+++ 241001/좌우로 움직이는 로봇/robot-moving-from-side-to-side.cpp
@@ -4,6 +4,11 @@
 
 using namespace std;
 
+int max(int a, int b) {
+    if (a > b) return a;
+    return b;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
     ios_base::sync_with_stdio(false);
@@ -18,43 +23,54 @@ int main() {
     int t;
     char d;
     int now = 1000000;
-    int time = 1;
+    int time1 = 1;
     for (int i = 0; i < n; i++) {
         cin >> t >> d;
 
         if (d == 'L') {
             while (t) {
                 t--;
-                A[time++] = --now;
+                A[time1++] = --now;
             }
         } else {
             while (t) {
                 t--;
-                A[time++] = ++now;
+                A[time1++] = ++now;
             }
         }
     }
 
     now = 1000000;
-    time = 1;
+    int time2 = 1;
     for (int i = 0; i < m; i++) {
         cin >> t >> d;
 
         if (d == 'L') {
             while (t) {
                 t--;
-                B[time++] = --now;
+                B[time2++] = --now;
             }
         } else {
             while (t) {
                 t--;
-                B[time++] = ++now;
+                B[time2++] = ++now;
             }
         }
     }
 
+    // A가 더 오래 움직인 경우
+    if (time1 > time2) {
+        for (int t = time2; t < time1; t++)
+            B[t] = B[time2-1];
+    } else {
+        for (int t = time1; t < time2; t++)
+            A[t] = A[time1-1];
+    }
+
+    int time = max(time1, time2);
+    
     int ans = 0;
-    for (int t = 2; t <= time; t++)
+    for (int t = 2; t < time; t++)
         if (A[t-1] != B[t-1] && A[t] == B[t]) ans++;
 
     cout << ans;
