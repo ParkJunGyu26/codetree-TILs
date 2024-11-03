a58da5ca8c140573f56f5d51cbbb24adf9b71c60
박준규 <81559637+ParkJunGyu26@users.noreply.github.com>
2024-11-03 14:21:20 +0900

    Title: 작은 구슬의 이동, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241103/작은 구슬의 이동/small-marble-movement.cpp
+++ 241103/작은 구슬의 이동/small-marble-movement.cpp
@@ -1,13 +1,8 @@
 #include <iostream>
 using namespace std;
 
-// 아래, 오른쪽, 왼쪽, 위
 int dx[4] = {0, 1, -1, 0};
 int dy[4] = {1, 0, 0, -1};
-
-bool InRange(int x, int y, int n) {
-    return (0 <= x && x < n && 0 <= y && y < n);
-}
 
 int graph[51][51];
 
@@ -17,26 +12,28 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int n, t, r, c;
-    char d; // 이동 방향
-    cin >> n >> t;
-    cin >> r >> c >> d;
+    int n, t, r, c, dir;
+    char d;
+    cin >> n >> t >> r >> c >> d;
 
-    int dir;
-    if (d == 'U') dir = 3; // 위로
-    else if (d == 'D') dir = 0; // 아래
-    else if (d == 'R') dir = 1; // 오른쪽
-    else dir = 2; // 왼쪽
+    r--;
+    c--;
 
-    for (int i = 0; i < t; i++) {
+    if (d == 'U') dir = 3;
+    else if (d == 'D') dir = 0;
+    else if (d == 'R') dir = 1;
+    else dir = 2;
+
+    for (int i = 0 ; i < t; i++) {
         int nx = c + dx[dir];
         int ny = r + dy[dir];
 
-        if (!InRange(nx, ny, n))
+        if (-1 < nx && nx < n && -1 < ny && ny < n) {
+            c = nx;
+            r = ny;
+        } else {
             dir = 3 - dir;
-        
-        r = ny;
-        c = nx;
+        }
     }
 
     cout << r+1 << " " << c+1;
